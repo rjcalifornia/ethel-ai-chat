f23050d3df56e1fb9404b198d208286c70d72a0a
rjcalifornia <rodolfohernan20@gmail.com>
2024-08-30 15:45:14 -0600

    Changes to be committed: 	modified:   lib/ui/screens/chat.dart

--- lib/ui/screens/chat.dart
+++ lib/ui/screens/chat.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'dart:convert';
+import 'dart:math';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -8,8 +13,44 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<types.Message> _messages = [];
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
+
+  String randomString() {
+    final random = Random.secure();
+    final values = List<int>.generate(16, (i) => random.nextInt(255));
+    return base64UrlEncode(values);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+          child: Chat(
+            messages: _messages,
+            onSendPressed: _handleSendPressed,
+            user: _user,
+            showUserAvatars: true,
+          ),
+        ));
+  }
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
   }
 }
