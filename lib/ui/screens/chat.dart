d0c13f65d67292d3477084f873a7ee0b2e38ce50
rjcalifornia <rodolfohernan20@gmail.com>
2024-09-13 10:57:48 -0600

    Changes to be committed: 	modified:   lib/ui/screens/chat.dart

--- lib/ui/screens/chat.dart
+++ lib/ui/screens/chat.dart
@@ -115,7 +115,90 @@ class _ChatScreenState extends State<ChatScreen> {
                   backgroundColor: Color(0xffFAFAFA),
                 ),
                 messages: _messages,
-                emptyState: EmptyStateWidget(),
+                emptyState: SingleChildScrollView(
+                    child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Center(
+                        child: Column(
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Image.asset("images/ethel-ai-logo.png"),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "Ethel AI",
+                          style: TextStyle(fontSize: 28),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const Icon(
+                          Icons.book_outlined,
+                          size: 40,
+                        ),
+                        const Text(
+                          "Ejemplos",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(
+                                width: 190,
+                                height: 88,
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5.0)),
+                                      backgroundColor: const Color(0xffF7F6F9)),
+                                  child: const Text(
+                                    "¿Quien es el Presidente de El Salvador?",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  onPressed: () {
+                                    _handleSendPressed(const types.PartialText(
+                                        text:
+                                            "¿Quien es el Presidente de El Salvador?"));
+                                  },
+                                )),
+                            SizedBox(
+                                width: 190,
+                                height: 88,
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5.0)),
+                                      backgroundColor: const Color(0xffF7F6F9)),
+                                  child: const Text(
+                                    "Dame el codigo para crear una funcion asincrona en NodeJS.",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  onPressed: () {
+                                    _handleSendPressed(const types.PartialText(
+                                      text:
+                                          "Dame el codigo para crear una funcion asincrona en NodeJS.",
+                                    ));
+                                  },
+                                )),
+                          ],
+                        )
+                      ],
+                    ))
+                  ],
+                )),
                 l10n: const ChatL10nEn(
                     inputPlaceholder: "Preguntale algo a la AI",
                     isTyping: "está escribiendo..."),
